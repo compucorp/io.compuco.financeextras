c79f697217da154539ca7df23b2af1733868d063
Erawat <erawat@gmail.com>
2022-05-17 10:23:28 +0100

    CIVIPLUS-774: Implement PaymentProcessor class

---
+++ Civi/Financeextras/Payment/PaymentProcessor.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Civi\Financeextras\Payment;
+
+/**
+ * PaymentProcessor
+ * @package Civi\Financeextras\Payment
+ */
+class PaymentProcessor {
+
+  /**
+   * @var null|PaymentProcessor
+   */
+  private static ?PaymentProcessor $singleton = NULL;
+
+  /**
+   * @var array
+   */
+  private array $cache = [];
+
+  /**
+   * @return \Civi\Financeextras\Payment\PaymentProcessor
+   */
+  public static function singleton(): PaymentProcessor {
+    if (!self::$singleton) {
+      self::$singleton = new self();
+    }
+    return self::$singleton;
+  }
+
+  /**
+   * Gets refund processor IDs.
+   *
+   * @param bool $force
+   *
+   * @return array
+   * @throws \CRM_Core_Exception
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function getRefundProcessorIDs(bool $force = TRUE): array {
+    if (!isset($this->cache['processor_ids']) || $force) {
+      $processors = civicrm_api3('PaymentProcessor', 'get', [
+        'sequential' => 1,
+        'is_active' => 1,
+        'is_test' => 0,
+      ])['values'];
+
+      $refundProcessorIDs = [];
+      foreach ($processors as $processor) {
+        $processor = \Civi\Payment\System::singleton()->getById($processor['id']);
+        if ($processor->supportsRefund()) {
+          $refundProcessorIDs[] = $processor->getID();
+        }
+      }
+
+      $this->cache['processor_ids'] = $refundProcessorIDs;
+    }
+
+    return $this->cache['processor_ids'];
+  }
+
+}
