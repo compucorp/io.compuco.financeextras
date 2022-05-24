f55cb950fed4136039d8e8dac4e84c979f2e3bb1
Erawat <erawat@gmail.com>
2022-05-24 13:32:36 +0100

    CIWEM-15: Add Fabricators

---
+++ tests/phpunit/Civi/Financeextras/Test/Fabricator/PaymentProcessorFabricator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Civi\Financeextras\Test\Fabricator;
+
+class PaymentProcessorFabricator extends AbstractBaseFabricator {
+
+  /**
+   * Entity's name.
+   *
+   * @var string
+   */
+  protected static $entityName = 'PaymentProcessor';
+
+  /**
+   * Array if default parameters to be used to create a contact.
+   *
+   * @var array
+   */
+  protected static $defaultParams = [];
+
+
+}
