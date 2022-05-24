8d60eaf9631ef854f3a01fe027066a8f17605c5b
Erawat <erawat@gmail.com>
2022-05-24 15:36:38 +0100

    CIVIPLUS-775: Add PaymentProcessorTest

---
+++ tests/phpunit/Civi/Financeextras/Refund/PaymentProcessorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Civi\Financeextras\Test\Fabricator\PaymentProcessorFabricator;
+use Civi\Financeextras\Refund\PaymentProcessor;
+
+/**
+ * Tests for the PaymentProcessor class.
+ *
+ * @group headless
+ */
+class PaymentProcessorTest extends BaseHeadlessTest {
+
+  public function testGetRefundProcessorIDs() {
+    $dummyProcessor = $this->fabricateDummyProcessor();
+
+    $paymentProcessor = new PaymentProcessor();
+    $processorIDs = $paymentProcessor->getRefundProcessorIDs();
+    $this->assertCount(1, $processorIDs);
+    $this->assertEquals($dummyProcessor['id'], $processorIDs[0]);
+
+  }
+
+  private function fabricateDummyProcessor() {
+    return PaymentProcessorFabricator::fabricate([
+      'payment_processor_type_id' => "Dummy",
+      'financial_account_id' => "Payment Processor Account",
+    ]);
+  }
+
+}
