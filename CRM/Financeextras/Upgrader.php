4457720ee35e920cec0a5d797af0fdb3aed69cb9
Omar Hussein <opetmar91@gmail.com>
2022-06-09 00:01:23 +0300

    CIWEM-18: Add "Accounts Receivable" payment method

--- CRM/Financeextras/Upgrader.php
+++ CRM/Financeextras/Upgrader.php
@@ -1,9 +1,46 @@
 <?php
-use CRM_Financeextras_ExtensionUtil as E;
+
+use Civi\Financeextras\Setup\Manage\AccountsReceivablePaymentMethod;
 
 /**
  * Collection of upgrade steps.
  */
 class CRM_Financeextras_Upgrader extends CRM_Financeextras_Upgrader_Base {
+
+  public function install() {
+    $creationSteps = [
+      new AccountsReceivablePaymentMethod(),
+    ];
+    foreach ($creationSteps as $step) {
+      $step->create();
+    }
+  }
+
+  public function enable() {
+    $steps = [
+      new AccountsReceivablePaymentMethod(),
+    ];
+    foreach ($steps as $step) {
+      $step->activate();
+    }
+  }
+
+  public function disable() {
+    $steps = [
+      new AccountsReceivablePaymentMethod(),
+    ];
+    foreach ($steps as $step) {
+      $step->deactivate();
+    }
+  }
+
+  public function uninstall() {
+    $removalSteps  = [
+      new AccountsReceivablePaymentMethod(),
+    ];
+    foreach ($removalSteps as $step) {
+      $step->remove();
+    }
+  }
 
 }
---
+++ Civi/Financeextras/Setup/Manage/AbstractManager.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Civi\Financeextras\Setup\Manage;
+
+/**
+ * Describes the interface for entities that are
+ * to be managed (created, removed ..etc) during the
+ * extension installations, disabling ..etc.
+ *
+ */
+abstract class AbstractManager {
+
+  /**
+   * Creates the entity.
+   */
+  abstract public function create();
+
+  /**
+   * Removes the entity.
+   */
+  abstract public function remove();
+
+  /**
+   * Deactivates the entity.
+   */
+  public function deactivate() {
+    $this->toggle(FALSE);
+  }
+
+  /**
+   * Activates the entity.
+   */
+  public function activate() {
+    $this->toggle(TRUE);
+  }
+
+  /**
+   * Activates/Deactivates the entity based on the passed status.
+   * You just need to implement this method for active() and deactivate()
+   * methods to work.
+   *
+   * @params boolean $status
+   *  True to activate the entity, False to deactivate the entity.
+   */
+  abstract protected function toggle($status);
+
+}
