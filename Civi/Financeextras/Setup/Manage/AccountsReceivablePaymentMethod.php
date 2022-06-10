e51d745597676f571179a7532f7da97bc35075c5
Omar Hussein <opetmar91@gmail.com>
2022-06-10 22:16:46 +0300

    CIWEM-18: Code improvments and style fixes

--- Civi/Financeextras/Setup/Manage/AccountsReceivablePaymentMethod.php
+++ Civi/Financeextras/Setup/Manage/AccountsReceivablePaymentMethod.php
@@ -2,19 +2,29 @@
 
 namespace Civi\Financeextras\Setup\Manage;
 
-class AccountsReceivablePaymentMethod extends AbstractManager {
+class AccountsReceivablePaymentMethod extends AbstractSetupManager {
+
+  /**
+   * The machine name of the payment method.
+   */
+  const NAME = 'accounts_receivable';
+
+  /**
+   * The title/label of the payment method.
+   */
+  const TITLE = 'Accounts Receivable';
 
   public function create(): void {
     $accountsReceivableFinancialAccountId = civicrm_api3('FinancialAccount', 'getvalue', [
       'return' => 'id',
-      'name' => 'Accounts Receivable',
+      'name' => self::TITLE,
     ]);
 
     if (empty($this->getAccountsReceivablePaymentMethodId())) {
       civicrm_api3('OptionValue', 'create', [
         'option_group_id' => 'payment_instrument',
-        'label' => 'Accounts Receivable',
-        'name' => 'accounts_receivable',
+        'label' => self::TITLE,
+        'name' => self::NAME,
         'financial_account_id' => $accountsReceivableFinancialAccountId,
         'is_reserved' => 1,
         'is_active' => 1,
@@ -33,7 +43,7 @@ class AccountsReceivablePaymentMethod extends AbstractManager {
 
   protected function toggle($status): void {
     civicrm_api3('OptionValue', 'get', [
-      'name' => "accounts_receivable",
+      'name' => self::NAME,
       'api.OptionValue.create' => ['id' => '$value.id', 'is_active' => $status],
     ]);
   }
@@ -43,11 +53,11 @@ class AccountsReceivablePaymentMethod extends AbstractManager {
       'sequential' => 1,
       'return' => ['id'],
       'option_group_id' => 'payment_instrument',
-      'name' => 'accounts_receivable',
+      'name' => self::NAME,
     ]);
 
     if (!empty($paymentMethod['id'])) {
-      return  $paymentMethod['id'];
+      return $paymentMethod['id'];
     }
 
     return NULL;
