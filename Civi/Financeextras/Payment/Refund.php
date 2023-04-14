06b08a8ee5f5236719bc6b7640ddcdd86d57fe3e
Erawat Chamanont <erawat@gmail.com>
2023-04-14 10:46:08 +0100

    CIVIPLUS-1272: Use get method and status name
    * CiviCRM will throw error if the result does not exist when using getSingle method
    * Change status ID to status name as the ID can be changed in the future in CiviCRM core

--- Civi/Financeextras/Payment/Refund.php
+++ Civi/Financeextras/Payment/Refund.php
@@ -33,13 +33,15 @@ class Refund {
     //API is likely to return more than one entity as CiviCRM separates financial
     //transactions for financial transaction for example, contribution amount is record
     //as one financial transaction and fee is record in another transaction.
-    $contribution = civicrm_api3('Contribution', 'getsingle', [
+    $contribution = civicrm_api3('Contribution', 'get', [
       'id' => $this->contributionID,
       'return' => ['contribution_status_id'],
-    ]);
-    if (isset($contribution['contribution_status_id']) && $contribution['contribution_status_id'] == 7) {
+    ])['values'][$this->contributionID];
+
+    if (isset($contribution['contribution_status_id']) && $contribution['contribution_status'] === 'Refunded') {
       return FALSE;
     }
+
     $entityFinancialTrxns = civicrm_api3('EntityFinancialTrxn', 'get', [
       'sequential' => 1,
       'entity_id' => $this->contributionID,
