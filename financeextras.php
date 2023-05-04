8e10ce9b00a7f2ed428eb3c5ac4fbb992c25dd6b
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-05-04 09:13:42 +0100

    CIWEMB-284: Upgrade boilerplate using civix 22.05.2

--- financeextras.php
+++ financeextras.php
@@ -15,15 +15,6 @@ function financeextras_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function financeextras_civicrm_xmlMenu(&$files) {
-  _financeextras_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -78,50 +69,6 @@ function financeextras_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function financeextras_civicrm_managed(&$entities) {
-  _financeextras_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Add CiviCase types provided by this extension.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function financeextras_civicrm_caseTypes(&$caseTypes) {
-  _financeextras_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Add Angular modules provided by this extension.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function financeextras_civicrm_angularModules(&$angularModules) {
-  // Auto-add module files from ./ang/*.ang.php
-  _financeextras_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function financeextras_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _financeextras_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_entityTypes().
  *
  * Declare entity types provided by this module.
@@ -132,9 +79,3 @@ function financeextras_civicrm_entityTypes(&$entityTypes) {
   _financeextras_civix_civicrm_entityTypes($entityTypes);
 }
 
-/**
- * Implements hook_civicrm_themes().
- */
-function financeextras_civicrm_themes(&$themes) {
-  _financeextras_civix_civicrm_themes($themes);
-}
