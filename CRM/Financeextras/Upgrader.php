bb995a5a38e101281e030b45ae7cfe1819ff2c37
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-05-23 07:18:12 +0100

    CIWEMB-289: Manage credit note status option group based on extension lifecycle

--- CRM/Financeextras/Upgrader.php
+++ CRM/Financeextras/Upgrader.php
@@ -1,9 +1,62 @@
 <?php
-use CRM_Financeextras_ExtensionUtil as E;
+
+use Civi\Financeextras\Setup\Manage\CreditNoteStatusManager;
 
 /**
  * Collection of upgrade steps.
  */
 class CRM_Financeextras_Upgrader extends CRM_Financeextras_Upgrader_Base {
+
+  /**
+   * Tasks to perform when the extension is installed.
+   */
+  public function install() {
+    $steps = [
+      new CreditNoteStatusManager(),
+    ];
+
+    foreach ($steps as $step) {
+      $step->create();
+    }
+  }
+
+  /**
+   * Tasks to perform when the extension is uninstalled.
+   */
+  public function uninstall() {
+    $steps = [
+      new CreditNoteStatusManager(),
+    ];
+
+    foreach ($steps as $step) {
+      $step->remove();
+    }
+  }
+
+  /**
+   * Tasks to perform when the extension is enanled.
+   */
+  public function enable() {
+    $steps = [
+      new CreditNoteStatusManager(),
+    ];
+
+    foreach ($steps as $step) {
+      $step->enable();
+    }
+  }
+
+  /**
+   * Tasks to perform when the extension is disabled.
+   */
+  public function disable() {
+    $steps = [
+      new CreditNoteStatusManager(),
+    ];
+
+    foreach ($steps as $step) {
+      $step->disable();
+    }
+  }
 
 }
