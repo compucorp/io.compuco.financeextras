b9b1d26601824694872767c4971f3aabc440743e
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-06-05 15:14:47 +0100

    CIWEMB-306: Include paid amount in Contribution get Api4 response

--- financeextras.php
+++ financeextras.php
@@ -14,6 +14,7 @@ function financeextras_civicrm_config(&$config) {
   _financeextras_civix_civicrm_config($config);
   Civi::dispatcher()->addListener('civi.api.respond', ['Civi\Financeextras\APIWrapper\SearchDisplayRun', 'respond'], -100);
   Civi::dispatcher()->addSubscriber(new Civi\Financeextras\Event\Subscriber\CreditNoteInvoiceSubscriber());
+  Civi::dispatcher()->addListener('civi.api.respond', ['Civi\Financeextras\APIWrapper\Contribution', 'respond'], -101);
 }
 
 /**
