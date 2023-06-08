9a9fe73c2b0131482739f5d5520c32adcb55f95f
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-06-08 16:46:09 +0100

    CIWEMB-301: Display credit note allocations

---
+++ ang/fe-creditnote/directives/creditnote-allocation-table.directive.js
@@ -0,0 +1,49 @@
+(function (angular) {
+  var module = angular.module('fe-creditnote');
+
+  module.directive('creditnoteAllocationTable', function () {
+    return {
+      restrict: 'E',
+      controller: 'creditnoteAllocationTableController',
+      templateUrl: '~/fe-creditnote/directives/creditnote-allocation-table.directive.html',
+      scope: {
+        creditNoteId: '@',
+      }
+    };
+  });
+
+  module.controller('creditnoteAllocationTableController', creditnoteAllocationTableController);
+
+  /**
+   * @param {object} $scope the controller scope
+   * @param {object} crmApi4 crm api V4 service
+   * @param {object} MoneyFormat service
+   */
+  function creditnoteAllocationTableController($scope, crmApi4, MoneyFormat) {
+    $scope.ts = CRM.ts();
+    $scope.crmUrl = CRM.url;
+    $scope.currency = 'GBP'
+    $scope.allocations = [];
+    $scope.total_credit = 0;
+    $scope.allocated_credit = 0;
+    $scope.remaining_credit = 0;
+    $scope.formatDate = CRM.utils.formatDate;
+    $scope.formatMoney = MoneyFormat.formatMoney;
+    $scope.hasAllocatePermission = CRM['fe-creditnote'].canEditContribution;
+
+    crmApi4('CreditNote', 'get', {
+      where: [["id", "=", $scope.creditNoteId]],
+      limit: 25,
+      chain: {"allocations":["CreditNoteAllocation", "get", {"where":[["credit_note_id", "=", "$id"]], "select":["*", "type_id:label", "contribution_id.invoice_number"]}]}
+    }).then(function(result) {
+      const creditnotes = result[0] ?? null;
+      $scope.currency = creditnotes.currency
+      $scope.allocations = creditnotes.allocations
+      $scope.total_credit = creditnotes.total_credit
+      $scope.remaining_credit = creditnotes.remaining_credit
+      $scope.allocated_credit = creditnotes.total_credit - creditnotes.remaining_credit
+    });
+
+  }
+
+})(angular);
