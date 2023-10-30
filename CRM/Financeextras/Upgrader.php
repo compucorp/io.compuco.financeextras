82f577dae830608296f88ad79bc2e3a3aaaed0b9
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-10-30 16:31:52 +0100

    ESEB-60: Add new Exchange Rate entity

--- CRM/Financeextras/Upgrader.php
+++ CRM/Financeextras/Upgrader.php
@@ -6,4 +6,14 @@ use CRM_Financeextras_ExtensionUtil as E;
  */
 class CRM_Financeextras_Upgrader extends CRM_Financeextras_Upgrader_Base {
 
+  /**
+   * This upgrade creates financeextras_exchange_rate table
+   */
+  public function upgrade_0001() {
+    $this->ctx->log->info('Applying update 0001');
+    $this->executeSqlFile('sql/upgrade_0001.sql');
+
+    return TRUE;
+  }
+
 }
---
+++ sql/auto_uninstall.sql
@@ -0,0 +1,10 @@
+---- /*******************************************************
+-- *
+-- * Clean up the existing tables-- *
+-- *******************************************************/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `financeextras_exchange_rate`;
+
+SET FOREIGN_KEY_CHECKS=1;
