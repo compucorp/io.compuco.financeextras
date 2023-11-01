74cf6ef8c43360cf8796d644c5eb3e9a65caae74
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-11-01 10:23:41 +0100

    ESEB-64: Allow user to delete exchange rate value

---
+++ CRM/Financeextras/Form/ExchangeRate/Delete.php
@@ -0,0 +1,51 @@
+<?php
+
+use CRM_Financeextras_ExtensionUtil as E;
+
+/**
+ * Exchange Rate Delete form controller
+ */
+class CRM_Financeextras_Form_ExchangeRate_Delete extends CRM_Core_Form {
+
+  /**
+   * Exchange rate value to delete
+   * @var int
+   */
+  public $id;
+
+  public function preProcess() {
+    CRM_Utils_System::setTitle('Delete Exchange Rate Value');
+
+    $this->id = CRM_Utils_Request::retrieve('id', 'Positive', $this);
+
+    $url = CRM_Utils_System::url('civicrm/exchange-rate', 'reset=1');
+    $session = CRM_Core_Session::singleton();
+    $session->replaceUserContext($url);
+  }
+
+  public function buildQuickForm() {
+    $this->addButtons([
+      [
+        'type' => 'submit',
+        'name' => E::ts('Yes'),
+      ],
+      [
+        'type' => 'cancel',
+        'name' => E::ts('No'),
+        'isDefault' => TRUE,
+      ],
+    ]);
+
+    parent::buildQuickForm();
+  }
+
+  public function postProcess() {
+    if (!empty($this->id)) {
+      \Civi\Api4\ExchangeRate::delete()
+        ->addWhere('id', '=', $this->id)
+        ->execute();
+      CRM_Core_Session::setStatus(E::ts('Exchange Rate Value deleted successfully.'), ts('Item deleted'), 'success');
+    }
+  }
+
+}
