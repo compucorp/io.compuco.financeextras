31978c0f37807e1ab22851256fd197b0d74948de
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2024-03-13 11:01:31 +0100

    PROD-155: Cree accounting entries for refund fee

--- tests/phpunit/Civi/Api4/Action/CreditNote/RefundActionTest.php
+++ tests/phpunit/Civi/Api4/Action/CreditNote/RefundActionTest.php
@@ -119,6 +119,140 @@ class Civi_Api4_CreditNote_RefundActionTest extends BaseHeadlessTest {
     $this->assertEquals(-1 * $amountToRefund, $lineItemSum);
   }
 
+  /**
+   * Tests that the expected entity financial transactions are created as part of the accounting entries for refund fee.
+   */
+  public function testExpectedRefundEntityFinancialAccountingEntriesAreCreatedForFee() {
+    $creditNote = $this->createCreditNote(500);
+    $amountToRefund = 200;
+    $feeAmount = 10;
+
+    $refundAllocation = $this->createRefund($creditNote['id'], $amountToRefund, [
+      'payment_instrument_id' => 3,
+      'fee_amount' => $feeAmount,
+      'check_number' => '1234',
+    ]);
+
+    $refundFinancialTrxn = \Civi\Api4\FinancialTrxn::get(FALSE)
+      ->addWhere('total_amount', '=', -1 * $amountToRefund)
+      ->addWhere('id', '=', $refundAllocation['financial_trxn_id'])
+      ->addWhere('net_amount', '=', -$amountToRefund - $feeAmount)
+      ->addWhere('payment_processor_id', 'IS NULL')
+      ->addWhere('payment_instrument_id', '=', 3)
+      ->addWhere('fee_amount', '=', $feeAmount)
+      ->addWhere('check_number', '=', '1234')
+      ->addWhere('check_number', '=', '1234')
+      ->addWhere('status_id', '=', 1)
+      ->addWhere('is_payment', '=', 1)
+      ->execute()
+      ->first();
+    $this->assertNotEmpty($refundFinancialTrxn);
+
+    $expectedToAccount = \CRM_Financial_BAO_FinancialAccount::getFinancialAccountForFinancialTypeByRelationship(
+      $creditNote['items'][0]['financial_type_id'],
+      'Expense Account is'
+    );
+
+    $expectedFromAccount = CRM_Financial_BAO_FinancialTypeAccount::getInstrumentFinancialAccount(3);
+
+    $refundFeeFinancialTrxn = \Civi\Api4\FinancialTrxn::get(FALSE)
+      ->addWhere('id', '=', $refundAllocation['fee_financial_trxn_id'])
+      ->addWhere('from_financial_account_id', '=', $expectedFromAccount)
+      ->addWhere('to_financial_account_id', '=', $expectedToAccount)
+      ->addWhere('currency', '=', $refundFinancialTrxn['currency'])
+      ->addWhere('trxn_id', '=', $refundFinancialTrxn['trxn_id'])
+      ->addWhere('trxn_date', '=', $refundFinancialTrxn['trxn_date'])
+      ->addWhere('payment_processor_id', 'IS NULL')
+      ->addWhere('payment_instrument_id', '=', 3)
+      ->addWhere('total_amount', '=', $feeAmount)
+      ->addWhere('net_amount', '=', $feeAmount)
+      ->addWhere('check_number', '=', '1234')
+      ->addWhere('fee_amount', '=', 0)
+      ->addWhere('is_payment', '=', 0)
+      ->addWhere('status_id', '=', 1)
+      ->execute()
+      ->first();
+    $this->assertNotEmpty($refundFeeFinancialTrxn);
+
+    $entityFinancialTrxn = \Civi\Api4\EntityFinancialTrxn::get(FALSE)
+      ->addWhere('entity_id', '=', $creditNote['id'])
+      ->addWhere('entity_table', '=', \CRM_Financeextras_DAO_CreditNote::$_tableName)
+      ->addWhere('financial_trxn_id', '=', $refundFeeFinancialTrxn['id'])
+      ->addWhere('amount', '=', $feeAmount)
+      ->execute()
+      ->first();
+    $this->assertNotEmpty($entityFinancialTrxn);
+
+    $financialItem = \Civi\Api4\FinancialItem::get(FALSE)
+      ->addWhere('entity_id', '=', $refundFeeFinancialTrxn['id'])
+      ->addWhere('entity_table', '=', 'civicrm_financial_trxn')
+      ->execute()
+      ->first();
+    $this->assertNotEmpty($financialItem);
+
+    $entityFinItemFinancialTrxn = \Civi\Api4\EntityFinancialTrxn::get(FALSE)
+      ->addWhere('entity_id', '=', $financialItem['id'])
+      ->addWhere('entity_table', '=', 'civicrm_financial_item')
+      ->addWhere('financial_trxn_id', '=', $refundFeeFinancialTrxn['id'])
+      ->addWhere('amount', '=', $feeAmount)
+      ->execute()
+      ->first();
+    $this->assertNotEmpty($entityFinItemFinancialTrxn);
+  }
+
+  /**
+   * Tests that the expected entity financial transactions is not created for refund fee.
+   */
+  public function testRefundEntityFinancialAccountingEntriesAreNotCreatedForFee() {
+    $creditNote = $this->createCreditNote(500);
+    $amountToRefund = 200;
+    $feeAmount = 0;
+
+    $refundAllocation = $this->createRefund($creditNote['id'], $amountToRefund, [
+      'payment_instrument_id' => 3,
+      'fee_amount' => $feeAmount,
+      'check_number' => '1234',
+    ]);
+
+    $refundFinancialTrxn = \Civi\Api4\FinancialTrxn::get(FALSE)
+      ->addWhere('total_amount', '=', -1 * $amountToRefund)
+      ->addWhere('id', '=', $refundAllocation['financial_trxn_id'])
+      ->addWhere('net_amount', '=', -$amountToRefund - $feeAmount)
+      ->addWhere('payment_processor_id', 'IS NULL')
+      ->addWhere('payment_instrument_id', '=', 3)
+      ->addWhere('fee_amount', '=', $feeAmount)
+      ->addWhere('check_number', '=', '1234')
+      ->addWhere('check_number', '=', '1234')
+      ->addWhere('status_id', '=', 1)
+      ->addWhere('is_payment', '=', 1)
+      ->execute()
+      ->first();
+    $this->assertNotEmpty($refundFinancialTrxn);
+
+    $expectedToAccount = \CRM_Financial_BAO_FinancialAccount::getFinancialAccountForFinancialTypeByRelationship(
+      $creditNote['items'][0]['financial_type_id'],
+      'Expense Account is'
+    );
+
+    $expectedFromAccount = CRM_Financial_BAO_FinancialTypeAccount::getInstrumentFinancialAccount(3);
+
+    $refundFeeFinancialTrxn = \Civi\Api4\FinancialTrxn::get(FALSE)
+      ->addWhere('id', '=', $refundAllocation['fee_financial_trxn_id'])
+      ->addWhere('from_financial_account_id', '=', $expectedFromAccount)
+      ->addWhere('to_financial_account_id', '=', $expectedToAccount)
+      ->addWhere('currency', '=', $refundFinancialTrxn['currency'])
+      ->addWhere('trxn_id', '=', $refundFinancialTrxn['trxn_id'])
+      ->addWhere('trxn_date', '=', $refundFinancialTrxn['trxn_date'])
+      ->addWhere('payment_processor_id', 'IS NULL')
+      ->addWhere('payment_instrument_id', '=', 3)
+      ->addWhere('total_amount', '=', $feeAmount)
+      ->addWhere('net_amount', '=', $feeAmount)
+      ->addWhere('check_number', '=', '1234')
+      ->execute()
+      ->first();
+    $this->assertEmpty($refundFeeFinancialTrxn);
+  }
+
   private function createRefund($creditNoteId, $amountToRefund = 200, $paymentParam = []) {
     return \Civi\Api4\CreditNote::refundAction()
       ->setId($creditNoteId)
@@ -131,6 +265,7 @@ class Civi_Api4_CreditNote_RefundActionTest extends BaseHeadlessTest {
         'pan_truncation' => "2333",
         'trxn_id' => mt_rand(1000, 6000),
         'fee_amount' => '1.50',
+        'currency' => 'GBP',
       ], $paymentParam))
       ->execute()
       ->first();
